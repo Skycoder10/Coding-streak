2c523d988c89d05dfff91eaf79bc3a7dd20c6a0f
Akash Mandal <50658274+Skycoder10@users.noreply.github.com>
2021-08-18 11:08:04 +0530

    Create basic_script.js

---
+++ basic_script.js
@@ -0,0 +1,8 @@
+document.querySelector(".hm-menu").addEventListener("click", () => {
+    console.log("I love");
+    document.querySelector(".container").classList.toggle("change");
+  });
+
+function myFunction() {
+  alert("I am working on it❤");
+}
